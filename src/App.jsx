cbeb102b813370a8fa9c010ffadf61e40aab8379
g1azed <victoria1230120@gmail.com>
2024-01-16 23:54:02 +0900

    App 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 import * as THREE from 'three'
 import { Physics } from '@react-three/cannon';
 import { Canvas, useThree} from '@react-three/fiber';
-import { Sky, Stats, OrbitControls, PointerLockControls} from '@react-three/drei';
+import { Sky, OrbitControls} from '@react-three/drei';
+
 import Controls from './components/Controls.jsx';
 import Plane from './components/area/Plane.jsx';
+import Exhibition from './components/area/Exhibition.jsx';
 import { FPV } from "./components/FPV";
 
 
@@ -16,10 +18,12 @@ function App() {
             <Canvas>
                 <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
                 <ambientLight intensity={0.5} />
+                {/* <OrbitControls /> */}
                 <FPV />
                 <Physics>
                     {/* 물리적인 영향을 받는 부분은 Physics 내부에 */}
                     <Controls />
+                    <Exhibition />
                     <Plane />
                 </Physics>
             </Canvas>
