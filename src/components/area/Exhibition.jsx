d7fd4c32d70b1031bec91460cb3b49392ecedc26
g1azed <victoria1230120@gmail.com>
2024-04-11 11:41:24 +0900

    Exhibition Update

--- src/components/area/Exhibition.jsx
+++ src/components/area/Exhibition.jsx
@@ -1,24 +1,74 @@
-import { useLoader } from "@react-three/fiber";
-import { Suspense, useMemo } from "react";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import {React, useState, useRef} from 'react'
+import { Canvas, Camera, useFrame, useLoader } from "@react-three/fiber";
+import { Color } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { useGLTF } from '@react-three/drei'
+import TestGltf from '../../assets/exhibition_sgvu.glb'
+import TestGltf2 from '../../assets/TeamObject/01_Bounce130.glb'
+import { Physics, useBox } from '@react-three/cannon';
 
-export default function Capybara() {
-    const { materials, scene } = useLoader(
-        GLTFLoader,
-        "https://gabriellazcano.com/models/capybara.glb"
-    ); // load the model
+function ExhibitionModel(props){
+    // const {nodes} = useGLTF(TestGltf);
+    // const [ref, api] = useTrimesh(
+    //     () => ({
+    //         args : [
+    //             nodes.Suzanne.geometry.attributes.position.array,
+    //             nodes.Suzanne.geometry.index.array,
+    //         ],
+    //         mass: 1,
+    //         ...props,
+    //     }),
+    //     useRef()
+    // )
 
-    useMemo(() => {
-        for (const material in materials) {
-        // iterate the materials
-        if (Object.prototype.hasOwnProperty.call(materials, material)) {
-            // change the color of all the materials (there's only one in this model)
-            materials[material].color.set("#bb6f3e");
-            // you can also change the color of a specific material if you know the name of the material
-        }
-        }
-    }, [materials]);
+    // return (
+    //     <group 
+    //         ref={ref}
+    //         {...props}
+    //         dispose={null}
+    //         onPointerDown={() => api.velocity.set(0,5,0)}
+    //     >
+    //         <mesh />
+    //     </group>
+    // )
+}
 
-    // return the primitive with our scene and changed materials
-    return <primitive object={scene} />;
+export default function Exhibition(props) {
+    const gltf = useLoader(GLTFLoader, TestGltf )
+    var white = new Color(0xffffff);
+
+    const [hovered, setHover] = useState(false);
+    const [active, setActive] = useState(false);
+
+    const [ref, api] = useBox(() => ({mass:1}))
+    // handle 3d model
+    // useFrame((state, delta, frame) => {
+    // const mesh = gltf.scene.children[0];
+    //     mesh.rotation.y = mesh.rotation.z += 0.01;
+    //     mesh.rotation.x = state.clock.getElapsedTime();
+    // });
+    
+    return (
+        <>
+            {/* <mesh geometry={ gltf.scene } >
+                <meshBasicMaterial color="#ffffff" />
+            </mesh> */}
+            <Physics>
+                <ambientLight intensity={0.5} />
+                <primitive
+                    object={gltf.scene}
+                    scale={1}
+                    color="#ffffff"
+                    onPointerOver={(e) => setHover(true)}
+                    onPointerOut={(e) => setHover(false)}
+                    ref={ref}
+
+                    // onClick={(e) =>
+                    // window.open("https://sketchfab.com/anthonyjamesgirdler")
+                    // } 
+                />
+            </Physics>
+
+        </>
+    )
 }
