723a57e74357cb54c1d3b922397cc30cca5a744c
g1azed <victoria1230120@gmail.com>
2024-04-11 11:41:34 +0900

    Plaen Update

--- src/components/area/Plane.jsx
+++ src/components/area/Plane.jsx
@@ -8,23 +8,25 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 const Plane = (props) => {
 
-
-    // const Exhibition_ = useRef();
-    // const {nodes, materials_} = useGLTF(Exhibition)
-
+    // const BounceExhibition = useLoader(GLTFLoader, './assets/TeamObject/03_omz.glb')
+    
     //usePlane으로 평면을 구성하고 ref를 통해 접근함
     const [ref] = usePlane(() => ({        
-        rotation: [-Math.PI / 2, 0, 0], // 평면의 기울기 otation 값을 수정해 기울기를 조절하자
+        rotation: [-Math.PI / 2, 0, 0], // 평면의 기울기 rotation 값을 수정해 기울기를 조절하자
         position: [0, 0, 0], // 평면의 위치값
     }));
 
     return(
-        <mesh ref={ref}> 
-        {/* mesh는 3D 화면을 구성하는 물체이며 ref를 통해 cannon을 통해 구성한 평면의 물리적인 속성을 적용한다.
-         물체(mesh)는 부피(Geometry)와 질감(Material)의 정보로 구성된다. */}
-            <boxGeometry attach="geometry" args={[100, 100]} /> 
-            <meshStandardMaterial attach="material" color="#303030" />
-        </mesh>
+        <>
+            {/* <primitive object={BounceExhibition.scene} position={[0,300,0]} children-0-castShadow/> */}
+            {/* <mesh ref={ref}> 
+                <boxGeometry attach="geometry" args={[10, 10]} /> 
+                <meshStandardMaterial attach="material" color="#fff" />
+            </mesh> */}
+            {/* mesh는 3D 화면을 구성하는 물체이며 ref를 통해 cannon을 통해 구성한 평면의 물리적인 속성을 적용한다.
+            물체(mesh)는 부피(Geometry)와 질감(Material)의 정보로 구성된다. */}
+        </>
+
     )
 };
 
